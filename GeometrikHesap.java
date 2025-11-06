912cfc1d92274321797440485637874a49b6c5f8
ulkutuanna <ulkusevgi123654@gmail.com>
2025-11-06 13:18:19 +0300

    Implement geometric calculations for circle and sphere
    This program calculates the area, circumference, diameter, volume, and surface area of a circle and sphere based on user input.

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,37 @@
+/*
+ * Ad Soyad: [Ülkü Tuanna Kara]
+ * Ogrenci No: [250541077]
+ * Tarih: [06.11.2025]
+ * Aciklama: Gorev 2 - Geometrik Hesaplayıcı
+ * 
+ * Bu program kullanicidan daire yarıcap uzunluğunu alır ve alanı, çevresi çapını ve kürenin hacmini, yüzey alanını hesaplar.
+ * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
+ */
+import java.util.Scanner;
 
+public class GeometrikHesap {
+    
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Dairenin yaricapini giriniz: ");
+            double yaricap = input.nextDouble();
+            
+            final double PI = 3.14;
+            
+            double alan = PI * yaricap * yaricap;
+            double cevre = 2 * PI * yaricap;
+            double cap = 2 * yaricap;
+            double hacim = (4.0 / 3.0) * PI * yaricap * yaricap * yaricap;
+            double yuzeyAlani = 4 * PI * yaricap * yaricap;
+            
+            System.out.println("\n === GEOMETRIK HESAPLAR ===");
+            System.out.printf("Yaricap: %.2f\n", yaricap);
+            System.out.printf("Dairenin Alani: %.2f\n", alan);
+            System.out.printf("Dairenin Cevresi: %.2f\n", cevre);
+            System.out.printf("Dairenin Capi: %.2f\n", cap);
+            System.out.printf("Kurenin Hacmi: %.2f\n", hacim);
+            System.out.printf("Kurenin Yuzey Alani: %.2f\n", yuzeyAlani);
+        }
+  }
+
+}
